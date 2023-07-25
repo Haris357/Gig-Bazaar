760e137d06383e37ec1a581b74eabce63c7572d7
Haris Muhammad Imran <harisimran7857@gmail.com>
2023-07-25 15:36:34 +0500

    Save to Database

--- server/router/auth.js
+++ server/router/auth.js
@@ -5,9 +5,27 @@ router.get('/', (req,res) => {
      res.send('Hello World router');
 });
 
+require('../db/conn');
+const User = require('../model/userSchema');
+
 router.post('/register',  (req,res) => {
-    console.log(req.body);
-    res.json({message:req.body});
+
+     const  {name,email,phone,work,password} = req.body;
+     if( !name || !email || !phone || !work || !password ){
+          return res.status(422).json({error: "Parameter Missing"});
+     }
+     User.findOne({email:email})
+          .then((userExist)=>{
+          if(userExist){
+               return res.status(422).json({error: "Email Already Exist"});
+          }
+          const user = new User({name,email,phone,work,password});
+
+          user.save().then(()=>{
+               res.status(201).json({ message: "User Registered Successfully" });
+          }).catch((err)=> res.status(500).json({error:"Registration Failed"}))
+     }).catch((err)=>{console.log(err);})
+
  });
 
 module.exports = router;
